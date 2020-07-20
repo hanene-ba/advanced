0705c2cc035f271d4ef8e5e2811d2b02ebcb4a87
hanene-ba <ba.hanene.96@gmail.com>
2020-07-20 18:24:28 +0200

    advanced

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    let  ProductTable = [{id:1 , name:"machine", price:60, category: "electronics"},
+                         {id:2, name:"Robe", price:80, category: "clothes"},
+                         {id:3, name:"smartPhone", price:100, category: "electronics"},
+                         {id:4, name:"Pull", price:50, category: "clothes"}
+  
+  ];
+   return (
+    <Table striped bordered hover variant="dark">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    {
+      ProductTable.map((item)=> 
+    <tbody>
+      <tr>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.price}</td>
+        <td>{item.category}</td>
+      </tr>
+    </tbody>
+      )
+  }
+  </Table>
+)
+};
 }
 
 export default App;
